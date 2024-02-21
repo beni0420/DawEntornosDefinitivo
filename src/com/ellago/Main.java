45e9e7b1f62e1b56689f316db451c597869b3a30
Javier <javierquinterodesantos@gmail.com>
2024-02-21 11:51:05 +0100

    Cambiamos el nombre de la variable miCuenta por Cuenta1

--- src/com/ellago/Main.java
+++ src/com/ellago/Main.java
@@ -9,21 +9,21 @@ package com.ellago;
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        CCuenta Cuenta1;
         double saldoActual;
 
-        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = miCuenta.estado();
+        Cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = Cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
         try {
-            miCuenta.retirar(2300);
+            Cuenta1.retirar(2300);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+            Cuenta1.ingresar(695);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
