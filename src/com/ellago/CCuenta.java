0f3f35cffa6156284a31a91a99a7f01b93204ef0
maria <maben0420@gmail.com>
2024-02-21 11:55:37 +0100

    Encapsulamos los atributos

--- src/com/ellago/CCuenta.java
+++ src/com/ellago/CCuenta.java
@@ -1,87 +1,83 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.ellago;
 
-
 public class CCuenta {
-
 
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
 
-
-    public CCuenta()
-    {
+    public CCuenta() {
     }
 
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
+        tipoInterés = tipo;
     }
 
-    public void asignarNombre(String nom)
-    {
+    public void asignarNombre(String nom) {
         setNombre(nom);
     }
 
-    public String obtenerNombre()
-    {
+    public String obtenerNombre() {
         return getNombre();
     }
 
-
-    public double estado()
-    {
+    public double estado() {
         return saldo;
     }
 
-
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
     }
 
-    public void retirar(double cantidad) throws Exception
-    {
+    public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
+            throw new Exception("No se puede retirar una cantidad negativa");
+        if (estado() < cantidad)
+            throw new Exception("No se hay suficiente saldo");
         saldo = saldo - cantidad;
     }
 
-
-    public String obtenerCuenta()
-    {
+    public String obtenerCuenta() {
         return cuenta;
     }
-
-
-    public String getNombre() {
-        return nombre;
-    }
-
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
 
     public double getTipoInterés() {
         return tipoInterés;
     }
 
-
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
+    }
+
+    // Métodos para encapsulación de atributos
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
     }
 }
